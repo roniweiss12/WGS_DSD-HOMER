ffaf9e567f3219ce0f609c5c02c4bf521ac7ebbe
roniweiss12 <84726455+roniweiss12@users.noreply.github.com>
2023-08-21 12:09:11 +0300

    Update catHomerResults.sh

--- scripts/catHomerResults.sh
+++ scripts/catHomerResults.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
 #this script takes a folder with HOMER results and concatenates
 # known and novel motifs into all_motifs.txt.
-HOMER_RSULTS="$1"
-OUTPUT_DIR="$2"
+HOMER_DIR="$1"
 
 # Check if the knownResults directory exists
-if [ ! -d "$HOMER_RSULTS/knownResults" ]; then
+if [ ! -d "$HOMER_DIR/knownResults" ]; then
     echo "Directory 'knownResults' not found."
     exit 1
 fi
 
 # Concatenate .motif files from knownResults/ directory
-cat $HOMER_RSULTS/knownResults/*.motif > $OUTPUT_DIR/all_motifs.txt
+cat $HOMER_DIR/knownResults/*.motif > $HOMER_DIR/all_motifs.txt
 
 # Concatenate homerMotifs.all.motifs
-cat $HOMER_RSULTS/homerMotifs.all.motifs >> $OUTPUT_DIR/all_motifs.txt
+cat $HOMER_DIR/homerMotifs.all.motifs >> $HOMER_DIR/all_motifs.txt
 
-echo "Concatenation complete. Result saved in $OUTPUT_DIR/all_motifs.txt."
+echo "Concatenation complete. Result saved in $HOMER_DIR/all_motifs.txt."
