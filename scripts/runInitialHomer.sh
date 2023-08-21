aeb2619389c0e6d21f102d19cc7008c5ebdef09e
roniweiss12 <84726455+roniweiss12@users.noreply.github.com>
2023-08-21 11:55:06 +0300

    Update runInitialHomer.sh

--- scripts/runInitialHomer.sh
+++ scripts/runInitialHomer.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-module load hurcs homer
+
 # Set the path to the HOMER program
 HOMER_PATH="" # Replace with the actual path to your HOMER binaries
 
@@ -12,26 +12,13 @@ HOMER_PATH="" # Replace with the actual path to your HOMER binaries
 
 # Input BED file (hg38 format)
 INPUT_BED="$1"  # Replace with the path to your hg38 BED file
-
-
-bed_name=`basename $INPUT_BED .bed `
-
-# Output directory for HOMER results
-OUTPUT_DIR="/HOMER_res/$bed_name"
-
+OUTPUT_DIR="HOMER_res"
 
 # Create the output directory if it doesn't exist
 mkdir -p "$OUTPUT_DIR"
 
-WORKDIR="/HOMER_res/"
- 
-# Step 0, doing stupid change, dunno why it is needed but...
-PEAK_FILE="$WORKDIR/$bed_name.homer"
-cp $INPUT_BED $PEAK_FILE
-
 # Step 1: Run HOMER to find motifs in the BED file
 
+echo  "$INPUT_BED" hg38 "$OUTPUT_DIR" -size 200 -mask -preparsedDir "$OUTPUT_DIR" 
 
-echo  "$INPUT_BED" hg38 "$OUTPUT_DIR" -size 200 -mask -preparsedDir "$WORKDIR" 
-
-findMotifsGenome.pl "$PEAK_FILE" hg38 "$OUTPUT_DIR" -size 200 -mask -preparsedDir "$WORKDIR"  -preparse
+findMotifsGenome.pl "$INPUT_BED" hg38 "$OUTPUT_DIR" -size 200 -mask -preparsedDir "$OUTPUT_DIR"  -preparse
