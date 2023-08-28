7100aa967fb92129bd8c70670d72cb74ca686a57
roniweiss12 <roniweiss12@gmail.com>
2023-08-28 10:47:44 +0300

    it worksgit add *

--- bin/runHomerInPeaks.sh
+++ bin/runHomerInPeaks.sh
@@ -2,15 +2,16 @@
 
 PEAK_FILE="$1"
 OUTPUT_DIR="$2" 
-WORK_DIR="$3"
+WORK_DIR="$1"
+MOTIFS_FILE="$4"
 
 # Check if the HOMER output file exists
-if [ ! -f "$OUTPUT_DIR/allMotifs.txt" ]; then
+if [ ! -f "$MOTIFS_FILE" ]; then
 
   echo "allMotifs.txt file not found."
   exit 1
 fi
 # Parse the HOMER output to extract motif occurrences and corresponding peaks
- 
-findMotifsGenome.pl "$PEAK_FILE" hg38 "$OUTPUT_DIR" -size 200 -mask -preparsedDir "$OUTPUT_DIR" -find $OUTPUT_DIR/allMotifs.txt > $OUTPUT_DIR/peaks_motif.tsv
+echo "$MOTIFS_FILE is found" 
+findMotifsGenome.pl "$PEAK_FILE" hg38 "$OUTPUT_DIR" -size 200 -mask -preparsedDir "$OUTPUT_DIR" -find $MOTIFS_FILE > $OUTPUT_DIR/peaks_motif.tsv
 echo "Motif discovery and peak analysis completed successfully."
